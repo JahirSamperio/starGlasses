851a4b0f3ac4599ef7736ffb35c25cd1836aee6b
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-05-02 10:57:21 -0600

    Añadiendo nivel de privilegio al usuario y agregar al carrito

--- backend/src/models/Usuario.js
+++ backend/src/models/Usuario.js
@@ -33,7 +33,11 @@ const Usuario = conexion.define('usuario', {
         allowNull: false
     },
     token: DataTypes.STRING,
-    confirmado: DataTypes.BOOLEAN
+    confirmado: DataTypes.BOOLEAN,
+    privilegio: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
 }, {
     hooks: {
         beforeCreate: async function(usuario){
